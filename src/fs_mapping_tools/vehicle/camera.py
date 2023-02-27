0575d6625abec8c0c1dcc01ee3d039c20d72a949
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-02-27 23:05:51 +0100

    Add detection area setting and representation
    Implemented a computation method for determining the detection area of the camera, based on its focal angle and distance.
    
    Also added a plotting method.

--- src/fs_mapping_tools/vehicle/camera.py
+++ src/fs_mapping_tools/vehicle/camera.py
@@ -7,8 +7,15 @@ Authors:
     Paulo Sanchez (@erlete)
 """
 
+from math import cos, pi, sin
+from typing import Any
 
+import matplotlib
+import matplotlib.pyplot as plt
 from bidimensional import Coordinate
+from bidimensional.polygons import Triangle
+
+from ..track.cones import Cone
 
 
 class Camera:
@@ -39,3 +46,77 @@ class Camera:
         self.orientation = orientation
         self.focal_angle = focal_angle
         self.focal_length = focal_length
+        self.set_detection_area()
+
+    def set_detection_area(self) -> None:
+        """Determine the detection area of the camera.
+
+        This method determines the detection area of the camera, which is
+        represented by a combination of triangles.
+        """
+
+        left_rot = self.orientation - self.focal_angle / 2
+        right_rot = self.orientation + self.focal_angle / 2
+
+        radius = self.position + Coordinate(
+            cos(self.orientation) * self.focal_length,
+            sin(self.orientation) * self.focal_length
+        )
+        left_boundary = self.position + Coordinate(
+            cos(left_rot) * self.focal_length,
+            sin(left_rot) * self.focal_length
+        )
+        right_boundary = self.position + Coordinate(
+            cos(right_rot) * self.focal_length,
+            sin(right_rot) * self.focal_length
+        )
+
+        self.detection_area = (
+            Triangle(
+                self.position,
+                left_boundary,
+                right_boundary
+            ), Triangle(
+                left_boundary,
+                right_boundary,
+                radius
+            )
+        )
+
+    def plot(self, ax: matplotlib.axes.Axes = None) -> None:
+        """Plot the camera and detection range.
+
+        Args:
+            ax (matplotlib.axes.Axes, optional): ax to plot the figure in.
+                Defaults to `plt.gca()` if None is passed as value.
+        """
+        ax = ax if ax is not None else plt.gca()
+
+        for triangle in self.detection_area:
+            triangle.plot(ax=ax, annotate=False)
+
+        self.position.plot(ax=ax, annotate=False)
+
+    def __contains__(self, element: Any):
+        """Determine whether an element is within the detection area.
+
+        Args:
+            element (Any): the element to be checked.
+
+        Returns:
+            bool: True if the element is within the detection area, False
+                otherwise.
+
+        Raises:
+            TypeError: if the element is not a Coordinate or Cone instance.
+        """
+        if not isinstance(element, (Coordinate, Cone)):
+            raise TypeError("element must be a Coordinate or Cone instance.")
+
+        if isinstance(element, Coordinate):
+            return any(element in triangle for triangle in self.detection_area)
+        else:
+            return any(
+                element.position in triangle
+                for triangle in self.detection_area
+            )
