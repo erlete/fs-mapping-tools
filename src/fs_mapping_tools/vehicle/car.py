3cac68b79b67359df6f857c813daaad5a06a89ad
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-01 18:58:44 +0100

    Update docstrings

--- src/fs_mapping_tools/vehicle/car.py
+++ src/fs_mapping_tools/vehicle/car.py
@@ -57,22 +57,59 @@ class CarState:
 
 
 class Wheel:
+    """Wheel representation class.
+
+    This class represent a wheel of the car. It includes the diameter and the
+    width of the wheel.
+
+    Attributes:
+        diameter (Union[int, float]): diameter of the wheel [m].
+        width (Union[int, float]): width of the wheel [m].
+    """
 
     __slots__ = ("diameter", "width")
 
     def __init__(self, diameter: Union[int, float],
                  width: Union[int, float]) -> None:
+        """Initialize a Wheel instance.
+
+        Args:
+            diameter (Union[int, float]): diameter of the wheel [m].
+            width (Union[int, float]): width of the wheel [m].
+        """
         self.diameter = diameter
         self.width = width
 
 
 class Axis:
+    """Direction axis representation class.
+
+    This class represent a direction axis of the car. It includes the wheels
+    that are attached to the axis, the distance between them  and the steering
+    angle.
+
+    Attributes:
+        left_wheel (Wheel): left wheel of the axis.
+        right_wheel (Wheel): right wheel of the axis.
+        steering_angle (Union[int, float]): steering angle of the axis [rad].
+        track (Union[int, float]): distance between the wheels of the axis [m].
+    """
 
     __slots__ = ("left_wheel", "right_wheel", "steering_angle", "track")
 
     def __init__(self, left_wheel: Wheel, right_wheel: Wheel,
                  steering_angle: Union[int, float],
                  track: Union[int, float]) -> None:
+        """Initialize an Axis instance.
+
+        Args:
+            left_wheel (Wheel): left wheel of the axis.
+            right_wheel (Wheel): right wheel of the axis.
+            steering_angle (Union[int, float]): steering angle of the axis
+                [rad].
+            track (Union[int, float]): distance between the wheels of the axis
+                [m].
+        """
         self.left_wheel = left_wheel
         self.right_wheel = right_wheel
         self.steering_angle = steering_angle
@@ -80,28 +117,70 @@ class Axis:
 
 
 class Direction:
+    """Direction system representation class.
+
+    This class represent the direction system of the car. It includes the front
+    and rear axis of the car and the distance between them.
+
+    Attributes:
+        front_axis (Axis): front axis of the car.
+        rear_axis (Axis): rear axis of the car.
+        wheelbase (Union[int, float]): distance between the front and rear axis
+            of the car [m].
+    """
 
     __slots__ = ("front_axis", "rear_axis", "wheelbase")
 
     def __init__(self, front_axis: Axis, rear_axis: Axis,
                  wheelbase: Union[int, float]) -> None:
+        """Initialize a Direction instance.
+
+        Args:
+            front_axis (Axis): front axis of the car.
+            rear_axis (Axis): rear axis of the car.
+            wheelbase (Union[int, float]): distance between the front and rear
+                axis of the car [m].
+        """
         self.front_axis = front_axis
         self.rear_axis = rear_axis
         self.wheelbase = wheelbase
 
 
 class Engine:
+    """Engine representation class.
+
+    This class represent the engine of the car. It includes valid value ranges
+    for the acceleration, speed and torque of the car.
+
+    Attributes:
+        speed (Tuple[Union[int, float], Union[int, float]]): valid speed range
+            of the car [m/s].
+        acceleration (Tuple[Union[int, float], Union[int, float]]): valid
+            acceleration range of the car [m/s^2].
+        torque (Tuple[Union[int, float], Union[int, float]]): valid torque
+            range of the car [Nm].
+    """
 
     __slots__ = ("acceleration", "speed", "torque")
 
     def __init__(
         self,
-        acceleration: Tuple[Union[int, float], Union[int, float]],
         speed: Tuple[Union[int, float], Union[int, float]],
+        acceleration: Tuple[Union[int, float], Union[int, float]],
         torque: Tuple[Union[int, float], Union[int, float]]
     ) -> None:
-        self.acceleration = acceleration
+        """Initialize an Engine instance.
+
+        Attributes:
+            speed (Tuple[Union[int, float], Union[int, float]]): valid speed
+                range of the car [m/s].
+            acceleration (Tuple[Union[int, float], Union[int, float]]): valid
+                acceleration range of the car [m/s^2].
+            torque (Tuple[Union[int, float], Union[int, float]]): valid torque
+                range of the car [Nm].
+        """
         self.speed = speed
+        self.acceleration = acceleration
         self.torque = torque
 
 
