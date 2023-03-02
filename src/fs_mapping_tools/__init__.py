52ed580659472a7b73a13dbb5f04c99749eb9fb0
Paulo Sánchez <dev.szblzpaulo@gmail.com>
2023-03-02 16:40:26 +0100

    Update import shorthands

--- src/fs_mapping_tools/__init__.py
+++ src/fs_mapping_tools/__init__.py
@@ -1,3 +1,3 @@
-from .track.cones import Cone, ConeArray
-from .vehicle.detection import Camera
-from .vehicle.car import Car, CarState, CarStructure
+from .track.cones import *
+from .vehicle.car import *
+from .vehicle.detection import *
